ec9b5460ee615192d81d956b6b6fbb4606d45f18
InfiniteLoop <hechen.dream@gmail.com>
2018-10-09 00:33:01 +0800

    add Overcast To DayOne Extension

--- Extensions/Overcast To DayOne.js
+++ Extensions/Overcast To DayOne.js
@@ -4,20 +4,26 @@ String.prototype.extract_nearest_content = function (start) {
         return "";
     }
 
-    var property_begin = this.indexOf("content=\"", start);    
+    var property_begin = this.indexOf("content=\"", start);
+    var property_end = this.indexOf("\"", property_begin + "content=\"".length);    
     if (property_begin != -1) {
         property_begin = property_begin + "content=\"".length;
     }
-    var property_end = this.indexOf("\">", property_begin);
 
     var content = this.substr(property_begin, property_end - property_begin);
     return content;
+};
+
+/// Check Overcast Link
+String.prototype.isOvercastURL = function() {
+    return this.indexOf("overcast.fm") != -1;
 };
 
 String.prototype.replaceEncoding = function() {
     return this.replace(/&amp;/g, "&").replace(/&#39;/g, "'").replace(/&mdash;/g, "-").replace(/&rsquo;/g, "’").replace(/&hellip;/g, "...");
 };
 
+/// Link from Action Extension or Clipboard.
 var link = $context.link;
 if (link == null) {
     link = $clipboard.link;
@@ -30,14 +36,9 @@ if (link == null) {
     }
 }
 
-/// Check Pocketcast Link
-String.prototype.isPocketcastURL = function () {
-    return this.indexOf("pca.st") != -1;
-};
-
-if (!link.isPocketcastURL()) {
+if (!link.isOvercastURL()) {
     $ui.alert({
-        title: "链接不是合法的 Pocketcast 链接",
+        title: "链接不是合法的 Overcast 链接",
         message: "",
     });
 
@@ -46,55 +47,57 @@ if (!link.isPocketcastURL()) {
 
 $ui.loading(true);
 
+console.log(link);
+
 var resp = await $http.get(encodeURI(link));
 var data = resp.data;
 if (data == null || resp.response.statusCode != 200) {
     $ui.loading(false);
-
+    
     $ui.alert({
         title: "链接无法获取内容",
         message: "",
     });
     return;
 }
-
 console.log(data);
 
-// parse title
+$ui.loading(false);
+
+// // parse title
 var title = data.extract_nearest_content(data.indexOf("og:title")).replaceEncoding();
 console.log(title);
 
-// parse album image
-// og:image        
-var img_url = data.extract_nearest_content(data.indexOf("og:image:secure_url"));
-var img_resp = await $http.get(encodeURI(img_url));
+// // parse album image
+// // og:image 
+var img_url = data.extract_nearest_content(data.indexOf("og:image"));
+console.log(img_url);
+img_url = img_url.replaceEncoding();
+console.log(img_url);
+
+var img_resp = await $http.get(img_url);
 var img_data = img_resp.rawData;
 $clipboard.image = img_data;
 
-// parse excert
+// // parse excert
 var desc = data.extract_nearest_content(data.indexOf("og:description")).replaceEncoding();
+console.log(desc);
 
-// create time 
-var episode_date_regex = /"episode_date\">(.+)<\/div>/;
-var episode_date_group = episode_date_regex.exec(data);
-console.log(episode_date_group[1]);
+var overcast_app_scheme = "app-argument=";
+var section_begin = data.indexOf(overcast_app_scheme);
+var scheme_begin = section_begin + overcast_app_scheme.length;
+var section_end = data.indexOf(",", scheme_begin);
+var scheme_section = data.substr(scheme_begin, section_end - scheme_begin);
+console.log(scheme_section);
 
 $ui.loading(false);
 
 
 // dayone://post?entry=Hello Self&journal=Day One
 var journal = title + "\n\n";
-journal += "Release Date: " + episode_date_group[1] + "\n";
 journal += "***\n" + desc + "\n***\n";
-journal += "[" + title + "]" + "(" + link + ")";
-
-
-var scheme = "dayone://post?entry=";
-scheme += journal;
-scheme += "&tags=pocketcast";
-scheme += "&tags=pocketcast";
-scheme += "&imageClipboard=1";
-
-console.log(scheme);
-
-$app.openURL(encodeURI(scheme));
+journal += "[" + title + "]" + "(" + link + ")\n\n\n";
+journal += "[Open In Overcast](" + scheme_section + ")";
+journal += "&tags=overcast";
+journal += "&imageClipboard=1";
+$app.openURL(encodeURI("dayone://post?entry=" + journal));
